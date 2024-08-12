ccd831de17f0813068111c35003335f2613d791c
Weslei Almeida Muniz <88255036+WesleiAlmeidaMuniz@users.noreply.github.com>
2024-08-12 09:21:39 -0300

    Atualização Projetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,23 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid dodgerblue}";
     document.body.appendChild(css);
 };
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight* 0.85;
+
+    item.forEach(Element => {
+        if (windowTop > Element.offsetTop){
+            Element.classList.add("animate");
+        } else{
+            Element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
